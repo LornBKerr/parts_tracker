ad69eb03be71e55550ed2c1fb12b41083abce2f0
LornBKerr <lornburtkerr@gmail.com>
2022-10-26 11:59:31 -0400

    Tests for Item and ItemSet

---
+++ tests/test_01_db_elements_item.py
@@ -0,0 +1,367 @@
+import pytest
+import os
+import sys
+
+src_path = os.path.join(os.path.realpath("."), "src")
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+from lbk_library import Dbal
+from db_elements import Item
+
+database = 'parts_test.db'
+
+def close_database(dbref):
+    dbref.sql_close()
+
+@pytest.fixture
+def open_database(tmpdir):
+    path = tmpdir.join(database)
+    dbref = Dbal()
+    # valid connection
+    dbref.sql_connect(path)
+    return dbref
+
+@pytest.fixture
+def create_items_table():
+    dbref = Dbal()
+    dbref.sql_connect('parts_test.db')
+    dbref.sql_query("DROP TABLE IF EXISTS 'items'")
+    create_table = 'CREATE TABLE IF NOT EXISTS "items"' + \
+                          '("record_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,' + \
+                          ' "part_number" TEXT NOT NULL,' + \
+                          ' "assembly" TEXT NOT NULL,' + \
+                          ' "quantity" INTEGER NOT NULL,' + \
+                          ' "condition" TEXT NOT NULL,' + \
+                          ' "installed" INTEGER NOT NULL DEFAULT 0,' + \
+                          ' "box" INTEGER DEFAULT NULL,' + \
+                          ' "remarks" TEXT DEFAULT NULL)'
+    dbref.sql_query(create_table)
+    return dbref
+
+# set item values from array of values
+item_values = dict({'record_id': 9876,
+                    'part_number': '13215',
+                    'assembly': 'P',
+                    'quantity': 4,
+                    'condition': 'New',
+                    'installed': 1,
+                    'remarks': 'test',
+                    'box': 5})
+
+
+def test_0101_constr(open_database):
+    dbref = open_database
+    item = Item(dbref)
+    assert isinstance(item, Item)
+    close_database(dbref)
+
+def test_0102_get_table(open_database):
+    dbref = open_database
+    item = Item(dbref)
+    assert item.get_table() == 'items'
+    close_database(dbref)
+
+def test_0103_get_dbref(open_database):
+    dbref = open_database
+    item = Item(dbref)
+    assert item.get_dbref() == dbref
+    close_database(dbref)
+
+def test_0104_get_set_record_id(open_database):
+    dbref = open_database
+    item = Item(dbref)
+    defaults = item.get_initial_values()
+#    assert item.get_record_id() == defaults['record_id']
+    item._set_property('record_id', None)
+    assert defaults['record_id'] == item.get_record_id()
+    item._set_property('record_id', item_values['record_id'])
+    assert item_values['record_id'] == item.get_record_id()
+    result = item.set_record_id(None)
+    assert not result['valid']
+    assert result['entry'] is None
+    result = item.set_record_id(-1)
+    assert not result['valid']
+    assert result['entry'] == -1
+    result = item.set_record_id(item_values['record_id'])
+    assert result['valid']
+    assert result['entry'] == item_values['record_id']
+    close_database(dbref)
+
+def test_0105_get_set_part_number(open_database):
+    dbref = open_database
+    item = Item(dbref)
+    defaults = item.get_initial_values()
+    item._set_property('part_number', item_values['part_number'])
+    assert item_values['part_number'] == item.get_part_number()
+    item._set_property('part_number', None)
+    assert item.defaults['part_number'] == item.get_part_number()
+    result = item.set_part_number(None)
+    assert not result['valid']
+    assert result['entry'] == None
+    result = item.set_part_number(item_values['part_number'])
+    assert result['valid']
+    assert result['entry'] == item_values['part_number']
+    assert result['entry'] == item.get_part_number()
+    close_database(dbref)
+
+def test_0106_get_set_assembly(open_database):
+    dbref = open_database
+    item = Item(dbref)
+    defaults = item.get_initial_values()
+    item._set_property('assembly', item_values['assembly'])
+    assert item_values['assembly'] == item.get_assembly()
+    item._set_property('assembly', None)
+    assert defaults['assembly'] == item.get_assembly()
+    result = item.set_assembly(None)
+    assert not result['valid']
+    assert result['entry'] is None
+    result = item.set_assembly(item_values['assembly'])
+    assert result['valid']
+    assert result['entry'] == item_values['assembly']
+    assert result['entry'] == item.get_assembly()
+    close_database(dbref)
+
+def test_0107_get_set_quantity(open_database):
+    dbref = open_database
+    item = Item(dbref)
+    defaults = item.get_initial_values()
+    item._set_property('quantity', item_values['quantity'])
+    assert item_values['quantity'] == item.get_quantity()
+    item._set_property('quantity', None)
+    assert defaults['quantity'] == item.get_quantity()
+    result = item.set_quantity(None)
+    assert not result['valid']
+    assert result['entry'] == None
+    result = item.set_quantity(-1)
+    assert not result['valid']
+    assert result['entry'] == -1
+    result = item.set_quantity(item_values['quantity'])
+    assert result['valid']
+    assert result['entry'] == item_values['quantity']
+    assert result['entry'] == item.get_quantity()
+    close_database(dbref)
+
+def test_0108_get_set_condition(open_database):
+    dbref = open_database
+    item = Item(dbref)
+    defaults = item.get_initial_values()
+    item._set_property('condition', item_values['condition'])
+    assert item_values['condition'] == item.get_condition()
+    item._set_property('condition', None)
+    assert defaults['condition'] == item.get_condition()
+    result = item.set_condition(None)
+    assert not result['valid']
+    assert result['entry'] is None
+    result = item.set_condition(item_values['condition'])
+    assert result['valid']
+    assert result['entry'] == item_values['condition']
+    assert result['entry'] == item.get_condition()
+    close_database(dbref)
+
+def test_0109_get_set_installed(open_database):
+    dbref = open_database
+    item = Item(dbref)
+    defaults = item.get_initial_values()
+    item._set_property('installed', item_values['installed'])
+    assert item_values['installed'] == item.get_installed()
+    item._set_property('installed', None)
+    assert defaults['installed'] == item.get_installed()
+    result = item.set_installed(None)
+    assert not result['valid']
+    assert result['entry'] == 0
+    result = item.set_installed(3)
+    assert not result['valid']
+    assert result['entry'] == 0
+    result = item.set_installed(item_values['installed'])
+    assert result['valid']
+    assert result['entry'] == item_values['installed']
+    assert result['entry'] == item.get_installed()
+    close_database(dbref)
+
+def test_0110_get_set_box(open_database):
+    dbref = open_database
+    item = Item(dbref)
+    defaults = item.get_initial_values()
+    item._set_property('box', item_values['box'])
+    assert item_values['box'] == item.get_box()
+    item._set_property('box', None)
+    assert defaults['box'] == item.get_box()
+    result = item.set_box(None)
+    assert not result['valid']
+    assert result['entry'] == None
+    result = item.set_box(-1)
+    assert not result['valid']
+    assert result['entry'] == -1
+    result = item.set_box(item_values['box'])
+    assert result['valid']
+    assert result['entry'] == item_values['box']
+    assert result['entry'] == item.get_box()
+    close_database(dbref)
+
+def test_0111_get_set_remarks(open_database):
+    dbref = open_database
+    item = Item(dbref)
+    defaults = item.get_initial_values()
+    item._set_property('remarks', None)
+    assert defaults['remarks'] == item.get_remarks()
+    result = item.set_remarks(None)
+    assert result['valid']
+    assert result['entry'] == ''
+    result = item.set_remarks(item_values['remarks'])
+    assert result['valid']
+    assert result['entry'] == item_values['remarks']
+    assert result['entry'] == item.get_remarks()
+    close_database(dbref)
+
+def test_0112_get_properties_type(open_database):
+    dbref = open_database
+    item = Item(dbref)
+    data = item.get_properties()
+    assert isinstance(data, dict)
+    close_database(dbref)
+
+def test_0113_item_get_default_property_values(open_database):
+    dbref = open_database
+    item = Item(dbref)
+    defaults = item.get_initial_values()
+    assert item.get_remarks() == defaults['remarks']
+    assert item.get_part_number() == defaults['part_number']
+    assert item.get_assembly() == defaults['assembly']
+    assert item.get_quantity() == defaults['quantity']
+    assert item.get_condition() == defaults['condition']
+    assert item.get_installed() == defaults['installed']
+    assert item.get_box() == defaults['box']
+    close_database(dbref)
+
+def test_0114_set_properties_from_dict(open_database):
+    # set Item from array
+    dbref = open_database
+    item = Item(dbref)
+    item.set_properties(item_values)
+    assert item_values['record_id'] == item.get_record_id()
+    assert item_values['part_number'] == item.get_part_number()
+    assert item_values['assembly'] == item.get_assembly()
+    assert item_values['quantity'] == item.get_quantity()
+    assert item_values['condition'] == item.get_condition()
+    assert item_values['installed'] == item.get_installed()
+    assert item_values['remarks'] == item.get_remarks()
+    assert item_values['box'] == item.get_box()
+    close_database(dbref)
+
+def test_0115_item_get_properties_size(open_database):
+    dbref = open_database
+    item = Item(dbref)
+    print(item._defaults)
+    print(item.get_properties())
+    assert len(item.get_properties()) == len(item_values)
+    close_database(dbref)
+
+def test_0116_item_from_dict(open_database):
+    # set Part from array
+    dbref = open_database
+    item = Item(dbref, item_values)
+    assert item_values['record_id'] == item.get_record_id()
+    assert item_values['part_number'] == item.get_part_number()
+    assert item_values['assembly'] == item.get_assembly()
+    assert item_values['quantity'] == item.get_quantity()
+    assert item_values['condition'] == item.get_condition()
+    assert item_values['installed'] == item.get_installed()
+    assert item_values['remarks'] == item.get_remarks()
+    assert item_values['box'] == item.get_box()
+    close_database(dbref)
+
+def test_0117_item_from__partial_dict(open_database):
+    dbref = open_database
+    del item_values['assembly']
+    item = Item(dbref, item_values)
+    assert item_values['record_id'] == item.get_record_id()
+    assert item_values['part_number'] == item.get_part_number()
+    assert '' == item.get_assembly()
+    assert item_values['quantity'] == item.get_quantity()
+    assert item_values['condition'] == item.get_condition()
+    assert item_values['installed'] == item.get_installed()
+    assert item_values['remarks'] == item.get_remarks()
+    assert item_values['box'] == item.get_box()
+    close_database(dbref)
+
+def test_0118_item_add(create_items_table):
+    dbref = create_items_table
+    item = Item(dbref, item_values)
+    item_id = item.add()
+    assert item_id == 1
+    assert item_id == item.get_record_id()
+    assert item_values['part_number'] == item.get_part_number()
+    assert item_values['assembly'] == item.get_assembly()
+    assert item_values['quantity'] == item.get_quantity()
+    assert item_values['condition'] == item.get_condition()
+    assert item_values['installed'] == item.get_installed()
+    assert item_values['remarks'] == item.get_remarks()
+    assert item_values['box'] == item.get_box()
+    close_database(dbref)
+
+def test_0119_item_read_db(create_items_table):
+    dbref = create_items_table
+    item = Item(dbref)
+    item.set_properties(item_values)
+    item_id = item.add()
+    assert item_id == 1
+        # read db for existing item
+    item2 = Item(dbref, 1)
+    assert item2.get_properties() is not None
+    assert item2.get_record_id() == 1
+    assert item_values['part_number'] == item2.get_part_number()
+    assert item_values['assembly'] == item2.get_assembly()
+    assert item_values['quantity'] == item2.get_quantity()
+    assert item_values['condition'] == item2.get_condition()
+    assert item_values['installed'] == item2.get_installed()
+    assert item_values['remarks'] == item2.get_remarks()
+    assert item_values['box'] == item2.get_box()
+        # read db for non-existing item
+    item3 = Item(dbref, 5)
+    assert isinstance(item3.get_properties(), dict)
+    assert len(item3.get_properties()) == len(item_values)
+        # Try direct read thru Element
+    item2.set_properties(item2.get_properties_from_db(None, None))
+    assert isinstance(item2.get_properties(), dict)
+    assert len(item2.get_properties()) == 0
+    close_database(dbref)
+
+def test_0120_item_update(create_items_table):
+    dbref = create_items_table
+    item = Item(dbref)
+    item.set_properties(item_values)
+    item_id = item.add()
+    assert item_id == 1
+    assert item_values['quantity'] == item.get_quantity()
+        # update item quantity
+    item.set_quantity(6)
+    result = item.update()
+    assert result
+    assert item.get_properties() is not None
+    assert item.get_record_id() == 1
+    assert item_values['part_number'] == item.get_part_number()
+    assert item_values['assembly'] == item.get_assembly()
+    assert not item_values['quantity'] == item.get_quantity()
+    assert item.get_quantity() == 6
+    assert item_values['condition'] == item.get_condition()
+    assert item_values['installed'] == item.get_installed()
+    assert item_values['remarks'] == item.get_remarks()
+    assert item_values['box'] == item.get_box()
+    close_database(dbref)
+
+def test_0121_item_delete(create_items_table):
+    dbref = create_items_table
+    item = Item(dbref)
+    item.set_properties(item_values)
+    item.add()
+        #delete item
+    result = item.delete()
+    assert result
+        #make sure it is really gone
+    item = Item(dbref, 1)
+    assert isinstance(item.get_properties(), dict)
+    assert len(item.get_properties()) == 8
+    close_database(dbref)
+
+#end test_01_db_elements_item.py
---
+++ tests/test_02_db_elements_item_set.py
@@ -0,0 +1,169 @@
+import pytest
+import os
+import sys
+
+src_path = os.path.join(os.path.realpath("."), "src")
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+from lbk_library import Dbal
+from db_elements import Item, ItemSet
+
+database = 'parts_test.db'
+
+def close_database(dbref):
+    dbref.sql_close()
+
+@pytest.fixture
+def create_items_table(tmpdir):
+    path = tmpdir.join(database)
+    dbref = Dbal()
+    dbref.sql_connect(path)
+    return dbref
+
+@pytest.fixture
+def create_items_table(tmpdir):
+    path = tmpdir.join(database)
+    dbref = Dbal()
+    dbref.sql_connect(path)
+    dbref.sql_query("DROP TABLE IF EXISTS 'items'")
+    create_table = 'CREATE TABLE IF NOT EXISTS "items"' + \
+                          '("record_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,' + \
+                          ' "part_number" TEXT NOT NULL,' + \
+                          ' "assembly" TEXT NOT NULL,' + \
+                          ' "quantity" INTEGER NOT NULL,' + \
+                          ' "condition" TEXT NOT NULL,' + \
+                          ' "installed" INTEGER NOT NULL DEFAULT 0,' + \
+                          ' "box" INTEGER DEFAULT NULL,' + \
+                          ' "remarks" TEXT DEFAULT NULL)'
+    dbref.sql_query(create_table)
+    return dbref
+
+def load_items_table(dbref):
+    columns = ['record_id', 'part_number', 'assembly', 'quantity', 'condition', 'installed', 'box', 'remarks']
+    value_set = [[ '1', '18V672', 'A', '1', 'Rebuild', '1', '', ''],
+                 ['2', 'BTB1108', 'B', '1', 'Usable', '0', '', ''],
+                 ['3', 'X036', 'D', '1', 'Usable', '0', '', ''],
+                 ['4', 'BTB1108', 'CA', '1', 'Usable', '0', '', ''],
+                 ['5', '22H1053', 'BB', '1', 'Usable', '0', '', ''],
+                 ['6', '268-090', 'BC', '1', 'Usable', '1', '', ''],
+                 ['8', 'BTB1108', 'BD', '1', 'Usable', '1', '', ''],
+                 ['9', 'X055', 'EB', '1', 'Usable', '1', '', ''],
+                 ['56', 'BULB-1895', 'JCIB', '2', 'Replace', '1', '', 'License Plate Lamp'],
+                 ['59', '158-520', 'JCIA', '2', 'Replace', '1', '', ''],
+                 ['70', 'BTB1108', 'CX', '1', 'Usable', '1', '', '']
+                ]
+    sql_query = {'type': 'INSERT', 'table': 'items'}
+    for values in value_set:
+        entries = {}
+        i = 0
+        while i < len(columns):
+            entries[columns[i]] = values[i]
+            i += 1
+        sql = dbref.sql_query_from_array(sql_query, entries)
+        dbref.sql_query(sql, entries)
+
+
+def test_0201_constructor(create_items_table):
+    dbref = create_items_table
+    item_set = ItemSet(dbref)
+    assert isinstance(item_set, ItemSet)
+    close_database(dbref)
+    
+def test_0202_get_dbref(create_items_table):
+    dbref = create_items_table
+    item_set = ItemSet(dbref)
+    assert item_set.get_dbref() == dbref
+    close_database(dbref)
+
+def test_0203_get_table(create_items_table):
+    dbref = create_items_table
+    item_set = ItemSet(dbref)
+    assert item_set.get_table() == 'items'
+    close_database(dbref)
+
+def test_0204_set_table(create_items_table):
+    dbref = create_items_table
+    item_set = ItemSet(dbref)
+    item_set.set_table('parts')
+    assert item_set.get_table() == 'parts'
+    close_database(dbref)
+    
+def test_0205_get_property_set(create_items_table):
+    dbref = create_items_table
+    item_set = ItemSet(dbref)
+    assert isinstance(item_set.get_property_set(), list)
+    close_database(dbref)
+    
+def test_0206_set_property_set_none(create_items_table):
+    dbref = create_items_table
+    item_set = ItemSet(dbref)
+    assert isinstance(item_set.get_property_set(), list)
+    item_set.set_property_set(None)
+    assert isinstance(item_set.get_property_set(), list)
+    assert len(item_set.get_property_set()) == 0
+    close_database(dbref)
+
+def test_0207_all_rows_empty(create_items_table):
+    dbref = create_items_table
+    item_set = ItemSet(dbref)
+    count_result = dbref.sql_query("SELECT COUNT(*) FROM " + item_set.get_table())
+    count = dbref.sql_fetchrow(count_result)['COUNT(*)']
+    assert count == len(item_set.get_property_set())
+    assert count == item_set.get_number_elements()
+    close_database(dbref)
+
+def test_0208_selected_rows(create_items_table):
+    dbref = create_items_table
+    load_items_table(dbref)
+    item_set = ItemSet(dbref,'part_number', 'BTB1108')
+    count_result = dbref.sql_query("SELECT COUNT(*) FROM " + item_set.get_table() + " WHERE part_number = 'BTB1108'")
+    count = dbref.sql_fetchrow(count_result)['COUNT(*)']
+    assert count == len(item_set.get_property_set())
+    close_database(dbref)
+
+def test_0209_ordered_selected_rows(create_items_table):
+    dbref = create_items_table
+    load_items_table(dbref)
+    item_set = ItemSet(dbref,'part_number', 'BTB1108', 'assembly')
+    count_result = dbref.sql_query("SELECT COUNT(*) FROM " + item_set.get_table() + " WHERE part_number = 'BTB1108'")
+    count = dbref.sql_fetchrow(count_result)['COUNT(*)']
+    selected_set = item_set.get_property_set()
+    assert count == len(selected_set)
+    for counter in range(0, count - 2):
+        item1 = selected_set[counter]
+        item2 = selected_set[counter + 1]
+        assert item1.get_assembly() < item2.get_assembly()
+    close_database(dbref)
+
+def test_10_selected_rows_limit(create_items_table):
+    dbref = create_items_table
+    load_items_table(dbref)
+    limit = 5
+    item_set = ItemSet(dbref, None, None, 'record_id', limit)
+    assert limit == len(item_set.get_property_set())
+    assert item_set.get_property_set()[0].get_record_id() == 1
+    close_database(dbref)
+
+def test_11_selected_rows_limit_offset(create_items_table):
+    dbref = create_items_table
+    load_items_table(dbref)
+    limit = 5
+    offset = 2
+    item_set = ItemSet(dbref, None, None, 'record_id', limit, offset)
+    assert limit == len(item_set.get_property_set())
+    assert item_set.get_property_set()[0].get_record_id() == 3
+    close_database(dbref)
+
+def test_12_iterator(create_items_table):
+    dbref = create_items_table
+    load_items_table(dbref)
+    limit = 5
+    item_set = ItemSet(dbref, None, None, 'record_id', limit)
+    i = 1
+    for item in item_set:
+        assert item.get_record_id() == i
+        i += 1
+    close_database(dbref)
+
+# end test_02_db_elements_item_set.py
