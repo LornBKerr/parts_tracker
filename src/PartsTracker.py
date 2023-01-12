86864991706fe9d863a5800b35b8c210937a1961
LornBKerr <lornburtkerr@gmail.com>
2023-01-12 11:28:29 -0500

    The driver for the program

---
+++ src/PartsTracker.py
@@ -0,0 +1,20 @@
+"""
+Run the 'Parts Tracker' Application
+
+File:       PartsTracker.py
+Author:     Lorn B Kerr
+Copyright:  (c) 2022, 2023 Lorn B Kerr
+License:    MIT, see file License 
+"""
+
+import sys
+
+from PyQt6.QtWidgets import QApplication
+
+from pages import MainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_window = MainWindow(app)
+    sys.exit(app.exec())
+
