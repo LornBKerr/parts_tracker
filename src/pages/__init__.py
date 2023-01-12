5402c3405f3c69e6707f25ad74a6fb38bce4ccfb
LornBKerr <lornburtkerr@gmail.com>
2023-01-12 11:27:06 -0500

    Include the active pages.

---
+++ src/pages/__init__.py
@@ -0,0 +1,17 @@
+"""
+The Parts Database pages collection providing the pages for the main window.
+
+This module contains the following classes
+     MainWindow extends QMainWindow)
+
+File       __init__.py
+Author     Lorn B Kerr
+Copyright  (c) 2023 Lorn B Kerr
+License    License:    MIT, see file License
+"""
+
+from .main_window import MainWindow
+from .assembly_tree_page import AssemblyTreePage
+from .parts_list_page import PartsListPage
+from .orders_list_page import OrdersListPage
+
