720a76345beb528b830d2db96bf2fa7185d68a20
LornBKerr <lornburtkerr@gmail.com>
2023-01-13 17:02:35 -0500

    Added a 'Part' to describe a single part on the car' and the test file for this part definition.

---
+++ src/elements/part.py
@@ -0,0 +1,277 @@
+"""
+Implement a single Part in the database.
+
+File:       part.py
+Author:     Lorn B Kerr
+Copyright:  (c) 2022, 2023 Lorn B Kerr
+License:    MIT, see file License
+"""
+
+from copy import deepcopy
+from typing import Any      #, Union
+
+from lbk_library import Dbal, Element
+from .item import Item
+from .item_set import ItemSet
+
+
+class Part(Element):
+    """
+    Implement a single Part in the database.
+    """
+    def __init__(self, dbref: Dbal, part_key: Any = None, column: str = None) ->None:
+        """
+        The constructor for a single Part.
+
+        The 'column' can be set to None, "record_id", or "part_number".
+        If not one of these three choices, both 'part_key' and 'column'
+        are set to "None" and a Part with default values is constructed.
+        The general defaults are: string values are set to
+        empty string, numeric values are set to 0, and logical values
+        are set to False.
+
+        If 'part_key' is not given, the 'column' is ignored and an empty
+        Part is constructed with all properties set to default values.
+
+        If part_key is a dict{} object, the properties of this Part are
+        set from the dict object.
+
+        If 'part_key' is given as a single value, it must be either an
+        'record_id' or an 'part_number' as indicated by 'column'. If
+        'column' is not given, 'record_id' is the default. The Part
+        will be constructed from the database for the specific value
+        given by 'column' and 'part_key'
+
+        Parameters:
+            dbref (Dbal): reference to the database holding the element
+            part_key (Mixed): the specific key of the Part being
+                constructed or an dict object of the values for an Part
+                for direct insertion into the properties array.
+                If a specific key, must be either an record_id or part
+                number value and must be consistent with the type given
+                by the column name.
+            column (String): Either 'record_id' or 'part_number',
+                default is None. Column name and part value must be
+                consistent.
+        """
+        super().__init__(dbref, "parts")
+
+        # Default values for the Part
+        self.defaults: dict[str, Any] = {
+            "record_id": 0,
+            "part_number": "",
+            "source": "",
+            "description": "",
+            "remarks": "",
+        }
+
+        self.set_initial_values(deepcopy(self.defaults))
+        self.clear_value_valid_flags()
+
+        if isinstance(part_key, dict):
+            # make sure there are no missing keys
+            for key in self.defaults:
+                if not key in part_key:
+                    part_key[key] = deepcopy(self.defaults[key])
+
+        if column is None:
+            column = "record_id"
+
+        if column not in ("record_id", "part_number"):
+            part_key = None
+            column = None
+
+        if isinstance(part_key, (int, str)):
+            part_key = self.get_properties_from_db(column, part_key)
+
+        if not part_key:
+            part_key = deepcopy(self.defaults)
+
+        self.set_properties(part_key)
+        self.set_initial_values(self.get_properties())
+        self.clear_value_changed_flags()
+    # end __init__()
+
+    def set_properties(self, properties: dict[str, Any]) -> None:
+        """
+        Set the values of the Part properties array.
+
+        Each property is validated for type and value within an acceptable
+        range, with unacceptable values set to default values. Properties
+        not part of the element are discarded.
+
+        Parameters:
+            properties (dict): holding the element values. Keys must
+            match the required keys of the element being modified,
+            properties may be sparse.
+        """
+        if properties is not None and isinstance(properties, dict):
+            super().set_properties(properties)
+
+            for key in properties.keys():
+                if key == "part_number":
+                    self.set_part_number(properties[key])
+                elif key == "source":
+                    self.set_source(properties[key])
+                elif key == "description":
+                    self.set_description(properties[key])
+
+        self.set_initial_values(self.get_properties())
+        self.clear_value_changed_flags()
+    # end set_properties()
+
+    def get_part_number(self) -> str:
+        """
+        Get the Part's part number.
+
+        Return:
+            (str) The part number as a string or, if None, an
+                empty string.
+        """
+        part_number = self._get_property("part_number")
+        if part_number is None:
+            part_number = self.defaults["part_number"]
+        return part_number
+    # end get_part_number()
+
+    def set_part_number(self, part_number: str) -> dict[str, Any]:
+        """
+        Set the Part's part number.
+
+		The part number is required and must be between 2 and 30
+		characters long. The valid and changed flags are updated based
+		on the result of the set operation.
+
+		Parameters:
+		    part_number (str): the new part number for the Part. If the
+		        supplied part number is not valid, the part number is
+		        set to the default empty string.
+
+		Returns:
+		    (dict) {['entry'] (str) the updated Part Number
+		            ['valid'] (bool) True if the operation suceeded,
+		                             false otherwise
+		            ['msg'] (Str) Error message if not valid
+        """
+        result = self.validate.text_field(
+            part_number, self.validate.REQUIRED, 2, 30
+        )
+        if result["valid"]:
+            self._set_property("part_number", result["entry"])
+        else:
+            self._set_property("part_number", self.defaults["part_number"])
+
+        self.update_property_flags(
+            "part_number", result["entry"], result["valid"]
+        )
+        return result
+    # end get_part_number()
+
+    def get_source(self) -> str:
+        """
+        Get the Part's source.
+
+        Return:
+            (str) The Part's source or, if None, an empty string
+        """
+        source = self._get_property("source")
+        if source is None:
+            source = self.defaults["source"]
+        return source
+    # end get_source()
+
+    def set_source(self, source: str) -> dict[str, Any]:
+        """
+        Set the Part's source.
+
+        The source is selected from a drop down box of the sources
+        (venders). The valid and changed flags are updated based on the
+        result of the set operation.
+
+        Parameters:
+            source (str): the new source for the Part. If the supplied
+                source is not valid, the source is set to an empty string.
+
+		Returns:
+		    (dict) {['entry'] (str) the updated Source
+		            ['valid'] (bool) True if the operation suceeded,
+		                             false otherwise
+		            ['msg'] (Str) Error message if not valid
+        """
+        result = self.validate.text_field(source, self.validate.REQUIRED)
+        if result["valid"]:
+            self._set_property("source", result["entry"])
+        else:
+            self._set_property("source", self.defaults["source"])
+
+        self.update_property_flags("source", result["entry"], result["valid"])
+        return result
+    # end set_source()
+
+    def get_description(self) -> str:
+        """
+        Get the Part's desciption.
+
+        Return:
+            (str) The Part's description or, if None, an empty string.
+        """
+        description = self._get_property("description")
+        if description is None:
+            description = self.defaults["description"]
+        return description
+    # end get_description()
+
+    def set_description(self, description: str) -> dict[str, Any]:
+        """
+        Set the Part's description.
+
+        The description is a text field of up to 255 characters.
+        The valid and changed flags are updated based on the result of
+        the set operation.
+
+        Parameters:
+            description (str): the description for the Part. If the
+            supplied description is not valid, the description is set to
+            the empty string
+
+		Returns:
+		    (dict) {['entry'] (str) the updated Description
+		            ['valid'] (bool) True if the operation suceeded,
+		                             false otherwise
+		            ['msg'] (Str) Error message if not valid
+        """
+        result = self.validate.text_field(description, self.validate.REQUIRED)
+        if result["valid"]:
+            self._set_property("description", result["entry"])
+        else:
+            self._set_property("description", self.defaults["description"])
+
+        self.update_property_flags(
+            "description", result["entry"], result["valid"]
+        )
+        return result
+    # end set_description()
+
+    def get_total_quantity(self) -> int:
+        """
+        Get the total quantity for this part number from the 'items' table.
+
+        The items table is searched for the items using this part number
+        then the quantities for each of these items is summed and returned.
+
+        Return (integer) the total quantity of this part number used.
+        """
+        item_set = ItemSet(
+            self.get_dbref(),
+            "part_number",
+            self.get_part_number(),
+            "record_id",
+        )
+        quantity = 0
+        for item in item_set:
+            quantity += item.get_quantity()
+        return quantity
+    # end get_total_quantity
+
+# end class Parts
+
---
+++ tests/test_03_elements_part.py
@@ -0,0 +1,328 @@
+"""
+Test the Part class.
+
+File:       item.py
+Author:     Lorn B Kerr
+Copyright:  (c) 2022 Lorn B Kerr
+License:    MIT, see file License
+"""
+
+import pytest
+import os
+import sys
+
+src_path = os.path.join(os.path.realpath("."), "src")
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+from lbk_library import Dbal
+from elements import Item, Part
+
+# ********************************************************************** 
+# Test setups
+# ********************************************************************** 
+
+database = 'parts_test.db'
+
+def close_database(dbref):
+    dbref.sql_close()
+
+@pytest.fixture
+def open_database(tmpdir):
+    path = tmpdir.join(database)
+    dbref = Dbal()
+    # valid connection
+    dbref.sql_connect(path)
+    return dbref
+
+def create_parts_table(dbref):
+    dbref.sql_query("DROP TABLE IF EXISTS 'parts'")
+    create_table = 'CREATE TABLE IF NOT EXISTS "parts"' + \
+	               ' ("record_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,' + \
+                   ' "part_number" TEXT NOT NULL,' + \
+                   ' "source" TEXT DEFAULT NULL,' + \
+                   ' "description" TEXT NOT NULL,' + \
+	               ' "remarks" TEXT DEFAULT NULL );'
+    result = dbref.sql_query(create_table)
+
+# used for testing part.get_total_quantity
+def create_items_table(dbref):
+    dbref.sql_query("DROP TABLE IF EXISTS 'items'")
+    create_table = 'CREATE TABLE IF NOT EXISTS "items"' + \
+                          '("record_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,' + \
+                          ' "part_number" TEXT NOT NULL,' + \
+                          ' "assembly" TEXT NOT NULL,' + \
+                          ' "quantity" INTEGER NOT NULL,' + \
+                          ' "condition" TEXT NOT NULL,' + \
+                          ' "installed" INTEGER NOT NULL DEFAULT 0,' + \
+                          ' "box" INTEGER DEFAULT NULL,' + \
+                          ' "remarks" TEXT DEFAULT NULL)'
+    dbref.sql_query(create_table)
+
+# used for testing part.get_total_quantity
+# set item values from array of values
+item_values = dict({'record_id': 0,
+                    'part_number': '13215',
+                    'assembly': 'P',
+                    'quantity': 4,
+                    'condition': 'New',
+                    'installed': 1,
+                    'remarks': 'test',
+                    'box': 5
+                   }
+                  )
+
+# set part values from array of values
+part_values = dict({'record_id': 9876,
+                    'part_number': '13215',
+                    'source': 'Moss',
+                    'description': 'bolt',
+                    'remarks': 'From local source'})
+
+# ********************************************************************** 
+# End Test setups
+# ********************************************************************** 
+
+# ********************************************************************** 
+# Begin Tests
+# ********************************************************************** 
+def test_01_constr(open_database):
+    dbref = open_database
+    part = Part(dbref)
+    assert type(part) == Part
+    close_database(dbref)
+    
+def test_02_get_table(open_database):
+    dbref = open_database
+    part = Part(dbref)
+    assert part.get_table() == 'parts'
+    close_database(dbref)
+    
+def test_03_get_dbref(open_database):
+    dbref = open_database
+    part = Part(dbref)
+    assert part.get_dbref() == dbref
+    close_database(dbref)
+
+def test_04_get_set_part_number(open_database):
+    dbref = open_database
+    part = Part(dbref)
+    defaults = part.get_initial_values()
+    part._set_property('part_number', part_values['part_number'])
+    assert part_values['part_number'] == part.get_part_number()
+    part._set_property('part_number', None)
+    assert part.defaults['part_number'] == part.get_part_number()
+    result = part.set_part_number(None)
+    assert not result['valid']
+    assert result['entry'] == None
+    result = part.set_part_number(part_values['part_number'])
+    assert result['valid']
+    assert result['entry'] == part_values['part_number']
+    assert result['entry'] == part.get_part_number()
+    close_database(dbref)
+
+def test_05_get_set_source(open_database):
+    dbref = open_database
+    part = Part(dbref)
+    defaults = part.get_initial_values()
+    part._set_property('source', part_values['source'])
+    assert part_values['source'] == part.get_source()
+    part._set_property('source', None)
+    assert part.defaults['source'] == part.get_source()
+    result = part.set_source(None)
+    assert not result['valid']
+    assert result['entry'] == None
+    result = part.set_source(part_values['source'])
+    assert result['valid']
+    assert result['entry'] == part_values['source']
+    assert result['entry'] == part.get_source()
+    close_database(dbref)
+
+def test_06_get_set_description(open_database):
+    dbref = open_database
+    part = Part(dbref)
+    defaults = part.get_initial_values()
+    part._set_property('description', part_values['description'])
+    assert part_values['description'] == part.get_description()
+    part._set_property('description', None)
+    assert part.defaults['description'] == part.get_description()
+    result = part.set_description(None)
+    assert not result['valid']
+    assert result['entry'] == None
+    result = part.set_description(part_values['description'])
+    assert result['valid']
+    assert result['entry'] == part_values['description']
+    assert result['entry'] == part.get_description()
+    close_database(dbref)
+
+def test_07_get_properties_type(open_database):
+    dbref = open_database
+    part = Part(dbref)
+    assert isinstance(part.get_properties(), dict)
+    close_database(dbref)
+
+def test_08_item_get_default_property_values(open_database):
+    dbref = open_database
+    part = Part(dbref)
+    defaults = part.get_initial_values()
+    assert part.get_part_number() == defaults['part_number']
+    assert part.get_source() == defaults['source']
+    assert part.get_description() == defaults['description']
+    close_database(dbref)
+
+def test_09_set_properties_from_dict(open_database):
+    # set Part from array
+    dbref = open_database
+    part = Part(dbref)
+    part.set_properties(part_values)
+    assert part_values['part_number'] == part.get_part_number()
+    assert part_values['source'] == part.get_source()
+    assert part_values['description'] == part.get_description()
+    close_database(dbref)
+
+def test_10_get_properties_size(open_database):
+    dbref = open_database
+    part = Part(dbref)
+    assert len(part.get_properties()) == len(part_values)
+    close_database(dbref)
+
+def test_11_part_from_dict(open_database):
+    # set Part from array
+    dbref = open_database
+    part = Part(dbref, part_values)
+    assert part_values['record_id'] == part.get_record_id()
+    assert part_values['part_number'] == part.get_part_number()
+    assert part_values['source'] == part.get_source()
+    assert part_values['description'] == part.get_description()
+    assert part_values['remarks'] == part.get_remarks()
+    close_database(dbref)
+    
+def test_12_part_from_partial_dict(open_database):
+    dbref = open_database
+    del part_values['source']
+    part = Part(dbref, part_values)
+    assert part_values['record_id'] == part.get_record_id()
+    assert part_values['part_number'] == part.get_part_number()
+    assert '' == part.get_source()
+    assert part_values['description'] == part.get_description()
+    assert part_values['remarks'] == part.get_remarks()
+    close_database(dbref)
+
+def test_13_bad_column_name(open_database):
+    dbref = open_database
+    part = Part(dbref, None, 'a_column')
+    defaults = part.get_initial_values()
+    assert part.get_record_id() == defaults['record_id']
+    assert part.get_part_number() == defaults['part_number']
+    assert part.get_source() == defaults['source']
+    assert part.get_description() == defaults['description']
+    assert part.get_remarks() == defaults['remarks']
+    close_database(dbref)
+
+def test_14_part_add(open_database):
+    dbref = open_database
+    create_parts_table(dbref)
+    part = Part(dbref, part_values)
+    record_id = part.add()
+    assert record_id == 1
+    assert record_id == part.get_record_id()
+    assert part_values['part_number'] == part.get_part_number()
+    assert part_values['source'] == part.get_source()
+    assert part_values['description'] == part.get_description()
+    assert part_values['remarks'] == part.get_remarks()
+    close_database(dbref)
+    
+def test_15_part_read_db(open_database):
+    dbref = open_database
+    create_parts_table(dbref)
+    part = Part(dbref)
+    part.set_properties(part_values)
+    record_id = part.add()
+    assert record_id == 1
+        #read db for existing part
+    part2 = Part(dbref, 1)
+    assert not part2 is None
+    assert not part2.get_properties() is None
+    assert record_id == part2.get_record_id()
+    assert part_values['part_number'] == part2.get_part_number()
+    assert part_values['source'] == part2.get_source()
+    assert part_values['description'] == part2.get_description()
+    assert part_values['remarks'] == part2.get_remarks()
+        # read db for non-existing part
+    part3 = Part(dbref, 5)
+    assert len(part3.get_properties()) == len(part_values)
+        # Try direct read thru Element
+    part2.set_properties(part2.get_properties_from_db(None, None))
+    assert len(part2.get_properties()) == 0
+    close_database(dbref)
+
+def test_16_part_update(open_database):
+    dbref = open_database
+    create_parts_table(dbref)
+    part = Part(dbref)
+    part.set_properties(part_values)
+    record_id = part.add()
+    assert record_id == 1
+    assert part_values['description'] == part.get_description()
+        # update part description
+    part.set_description("Bolt, #10")
+    result = part.update()
+    assert result
+    assert part.get_properties() is not None
+    assert part_values['part_number'] == part.get_part_number()
+    assert part_values['source'] == part.get_source()
+    assert not part_values['description'] == part.get_description()
+    assert "Bolt, #10" == part.get_description()
+    assert part_values['remarks'] == part.get_remarks()
+    close_database(dbref)
+
+def test_17_part_delete(open_database):
+    dbref = open_database
+    create_parts_table(dbref)
+    part = Part(dbref)
+    part.set_properties(part_values)
+    record_id = part.add()
+    assert record_id
+        #delete part
+    result = part.delete()
+    assert result
+        #make sure it is really gone
+    part = Part(dbref, part_values['part_number'])
+    assert isinstance(part.get_properties(), dict)
+    assert len(part.get_properties()) == len(part_values)
+
+def test_17_get_total_quantity(open_database):
+    dbref = open_database
+    create_parts_table(dbref)
+    # create items table with 4 entries, three with current part number
+    create_items_table(dbref)
+    assert dbref   
+    item = Item(dbref,item_values)
+    item.add()
+    total_quantity = item.get_quantity()
+    item_values['quantity'] = 2
+    item_values['assembly'] = 'R'
+    item = Item(dbref,item_values)
+    item.add()
+    total_quantity += item.get_quantity()
+    item_values['quantity'] = 3
+    item_values['assembly'] = 'RA'
+    item_values['part_number'] = '326-735'
+    item = Item(dbref,item_values)
+    item.add()
+    item_values['quantity'] = 7
+    item_values['assembly'] = 'PA'
+    item_values['part_number'] = '13215'
+    item = Item(dbref,item_values)
+    item.add()
+    total_quantity += item.get_quantity()
+         # check total quantity used
+    part = Part(dbref)
+    part.set_properties(part_values)
+    record_id = part.add()
+    assert record_id
+    quantity = part.get_total_quantity()
+    assert quantity == total_quantity
+    close_database(dbref)
+
+# end test_03_elements_part.py
