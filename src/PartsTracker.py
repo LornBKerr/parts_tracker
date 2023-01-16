607cd7b8d77d6f48b7403f96bfd2e6ddd523a6a2
LornBKerr <lornburtkerr@gmail.com>
2023-01-16 10:39:22 -0500

    Change single quotes to double quotes

--- src/PartsTracker.py
+++ src/PartsTracker.py
@@ -13,8 +13,7 @@ from PyQt6.QtWidgets import QApplication
 
 from pages import MainWindow
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     main_window = MainWindow(app)
     sys.exit(app.exec())
-
