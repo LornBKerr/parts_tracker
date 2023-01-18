95d43c434d4338420f25b5e51c2643d1c1ddac05
LornBKerr <lornburtkerr@gmail.com>
2023-01-18 12:26:47 -0500

    Added the element Condition and associated test file.

---
+++ src/elements/condition.py
@@ -0,0 +1,134 @@
+"""
+Implement a single Condition in the database.
+
+File:       condition.py
+Author:     Lorn B Kerr
+Copyright:  (c) 2023 Lorn B Kerr
+License:    MIT, see file License
+"""
+
+from copy import deepcopy
+from typing import Any  # , Union
+
+from lbk_library import Dbal, Element
+
+class Condition(Element):
+    """
+    Implement a single Condition in the database.
+    """
+
+    def __init__(self, dbref: Dbal, condition_key: str = None) -> None:
+        """
+        The constructor for a single Condition.
+
+        The requested Condition is keyed on the condition_key. It can be
+        a single integer value (the record_id) or a dict object
+        containing the properties of an Condition, or None.
+
+        If the condition_key is a single integer value, the Condition
+        will be retrieved from the database. If condition_key is a
+        dict object, the properties of this Condition are set from the
+        dict object. If the condition_key is not provided or the
+        database does not contain the requested condition, The Condition
+        is constructed from the default values. The condition_key dict
+        may be sparse and missing entries will be filled from the
+        default values.
+
+        Parameters:
+            dbref (Dbal): reference to the database holding the element
+            condition_key (str | dict): the record_id of the Condition
+                being constructed or an dict object of the values for a
+                Condition for direct insertion into the properties array.
+        """
+        super().__init__(dbref, "conditions")
+
+        # Default values for the Condition
+        self.defaults: dict(str, Any) = {
+            'record_id': 0,
+            'condition': '',
+        }
+
+        self.set_initial_values(deepcopy(self.defaults))
+        self.clear_value_valid_flags()
+        
+        if isinstance(condition_key, dict):
+            # make sure there are no missing keys
+            for key in self.defaults:
+                if not key in condition_key:
+                    condition_key[key] = deepcopy(self.defaults[key])
+
+        if isinstance(condition_key, (int, str)):
+            condition_key = self.get_properties_from_db('record_id', condition_key)
+
+        if not condition_key:
+            condition_key = deepcopy(self.defaults)
+
+        self.set_properties(condition_key)
+        self.set_initial_values(self.get_properties())
+        self.clear_value_changed_flags()
+    # end __init__()
+
+    def set_properties(self, properties: dict[str, Any]) -> None:
+        """
+        Set the values of the Condition properties array.
+
+        Each property is validated for type and value within an
+        acceptable range, with unacceptable values set to default
+        values. Properties not part of the element are discarded.
+
+        Parameteres:
+            properties (dict) the element values; keys must match the
+            required keys of the Condition being creates/modified
+        """
+        if properties is not None and isinstance(properties, dict):
+            # Handle the 'record_id' and 'remarks' entries
+            super().set_properties(properties)
+            # Handle all the other properties here
+            for key in properties.keys():
+                if key == 'condition':
+                    self.set_condition(properties[key])
+    # end set_properties()
+
+    def get_condition(self) -> str:
+        """
+        Get the condition for this Condition object
+
+        Returns:
+            (str) The Condition's condition or, if None, the
+                default value.
+        """
+        condition = self._get_property('condition')
+        if condition is None:
+            condition = self.defaults['condition']
+        return condition
+    # end get_condition(()
+
+    def set_condition(self, condition: str) -> dict[str, Any]:
+        """
+        Set the conditon for this Condition.
+
+        The valid and changed flags are updated based on the result of
+        the set operation.
+
+        Parameters:
+            condition ((String) the conditon for this Condition,
+                required and between 1 and 31 characters
+
+        Returns:
+            (dict)
+                ['entry'] - the updated storage box
+                ['valid'] - (bool) True if the operation suceeded,
+                    False otherwise
+                ['msg'] - (str) Error message if not valid
+        """
+        result = self.validate.text_field(condition, self.validate.REQUIRED, 1, 31)
+        if result['valid']:
+            self._set_property('condition', result['entry'])
+        else:
+            self._set_property('condition', self.defaults['condition'])
+        self.update_property_flags("condition", result["entry"], result["valid"])
+        return result
+    # end set_condition(()
+
+# end Condition
+
--- tests/elements/test_01_condition.py
+++ tests/elements/test_01_condition.py
@@ -55,128 +55,133 @@ def test_01_03_get_dbref(open_database):
     assert condition.get_dbref() == dbref
     close_database(dbref)
 
-def test_01_04_set_conditions(open_database):
+def test_01_04_set_condition(open_database):
     # set empty Condition
     dbref = open_database
     condition = Condition(dbref)
-        # 'condition': 'Moss USA' required
-    condition._set_property('condition', None);
-    assert condition.get_condition() == ""
+    defaults = condition.get_initial_values()
+    condition._set_property("condition", condition_values["condition"])
+    assert condition_values["condition"] == condition.get_condition()
+    condition._set_property("condition", None)
+    assert condition.defaults["condition"] == condition.get_condition()
     result = condition.set_condition(None)
-    assert not result['valid']
-    assert result['entry'] is None
-    assert condition.get_condition() == ''
-    result = condition.set_condition(condition_values['condition']) 
-    assert result['valid']
-    assert result['entry'] == condition_values['condition']
-    assert result['entry'] == condition.get_condition()
+    assert not result["valid"]
+    assert result["entry"] == None
+    result = condition.set_condition(condition_values["condition"])
+    assert result["valid"]
+    assert result["entry"] == condition_values["condition"]
+    assert result["entry"] == condition.get_condition()
+    close_database(dbref)
+
+def test_01_05_get_properties_type(open_database):
+    dbref = open_database
+    condition = Condition(dbref)
+    data = condition.get_properties()
+    assert type(data) == dict
     close_database(dbref)
 
 
-#def test_01_04_get_properties_type(open_database):
-#    dbref = open_database
-#    condition = Condition(dbref)
-#    data = condition.get_properties()
-#    assert type(data) == dict
-#    close_database(dbref)
-#
-#def test_01_05_get_properties_size(open_database):
-#    dbref = open_database
-#    condition = Condition(dbref)
-#    data = condition.get_properties()
-#    assert len(data) == 2
-#    close_database(dbref)
-3
-#def test_01_06_get_property_values_none(open_database):
-#    dbref = open_database
-#    condition = Condition(dbref)
-#    defaults = condition.get_initial_values()
-#    assert condition.get_record_id() == defaults['record_id']
-#    assert condition.get_condition() == defaults['condition']
-#    condition._set_property('record_id', None)
-#    close_database(dbref)
-#def test_01_08_set_properties_from_dict(open_database):
-#    # set Condition from array
-#    dbref = open_database
-#    condition = Condition(dbref)
-#    condition.set_properties(condition_values)
-#    assert condition_values['record_id'] == condition.get_record_id()
-#    assert condition_values['condition'] == condition.get_condition()
-#    close_database(dbref)
-#
-#def test_01_09_constructor_missing_key(open_database):
-#    dbref = open_database
-#    values = {'record_id': 15}
-#    condition = Condition(dbref, values)
-#    assert values['record_id'] == condition.get_record_id()
-#    assert "" == condition.get_condition()
-#    close_database(dbref)
-#
-#def test_01_10_add(create_conditions_table):
-#    dbref = create_conditions_table
-#    condition = Condition(dbref, condition_values)
-#    record_id = condition.add()
-#    assert record_id == 1
-#    assert record_id == condition.get_record_id()
-#    assert condition_values['condition'] == condition.get_condition()
-#    close_database(dbref)
-#    
-#def test_01_11_read_db(create_conditions_table):
-#    dbref = create_conditions_table
-#    condition = Condition(dbref)
-#    condition.set_properties(condition_values)
-#    record_id = condition.add()
-#    assert record_id == 1
-#        #read db for existing part
-#    condition2 = Condition(dbref, record_id)
-#    assert record_id == condition2.get_record_id()
-#    assert condition_values['condition'] == condition2.get_condition()
-#        # read db for non-existing part
-#    condition3 = Condition(dbref, 5)
-#    assert isinstance(condition3.get_properties(), dict)
-#    assert len(condition3.get_properties()) == len(condition_values)
-#        # Try direct read thru Element
-#    condition2.set_properties(condition2.get_properties_from_db(None, None))
-#    assert isinstance (condition2.get_properties(), dict)
-#    assert len(condition2.get_properties()) == 0
-#    close_database(dbref)
-#
-#def test_01_12_update(create_conditions_table):
-#    dbref = create_conditions_table
-#    condition = Condition(dbref)
-#    condition.set_properties(condition_values)
-#    record_id = condition.add()
-#    assert record_id == 1
-#    assert condition_values['condition'] == condition.get_condition()
-#        
-#        # update condition
-#    condition.set_condition("British Wiring")
-#    result = condition.update()
-#    assert result
-#    assert condition.get_properties() is not None
-#    assert record_id == condition.get_record_id()
-#    assert not condition_values['condition'] == condition.get_condition()
-#    assert "British Wiring" == condition.get_condition()
-#    close_database(dbref)
-#
-#def test_01_13_delete(create_conditions_table):
-#    dbref = create_conditions_table
-#    condition = Condition(dbref)
-#    condition.set_properties(condition_values)
-#    record_id = condition.add()
-#        #delete part
-#    result = condition.delete()
-#    assert result
-3        #make sure it is really gone
-#    condition2 = Condition(dbref, condition_values['record_id'])
-#    assert isinstance (condition2.get_properties(), dict)
-#    assert len(condition2.get_properties()) == len(condition_values)
-#    close_database(dbref)
-#
-#def test_01_14_cleanup(open_database):
-#    dbref = open_database
-#    result = dbref.sql_query("DROP TABLE IF EXISTS 'conditions'")
-#    assert result
-#    close_database(dbref)
-#    delete_database()
-#
+def test_01_06_get_default_property_values(open_database):
+    dbref = open_database
+    condition = Condition(dbref)
+    defaults = condition.get_initial_values()
+    assert condition.get_record_id() == defaults["record_id"]
+    assert condition.get_condition()  == defaults["condition"]
+    close_database(dbref)
+
+
+def test_01_07_set_properties_from_dict(open_database):
+    # set Condition from array
+    dbref = open_database
+    condition = Condition(dbref)
+    condition.set_properties(condition_values)
+    assert condition_values['record_id'] == condition.get_record_id()
+    assert condition_values['condition'] == condition.get_condition()
+    close_database(dbref)
+
+
+def test_01_08_get_properties_size(open_database):
+    dbref = open_database
+    condition = Condition(dbref)
+    data = condition.get_properties()
+    assert len(data) == 2
+    close_database(dbref)
+
+def test_01_09_condition_from_dict(open_database):
+    dbref = open_database
+    condition = Condition(dbref, condition_values)
+    assert condition_values["record_id"] == condition.get_record_id()
+    assert condition_values["condition"] == condition.get_condition()
+    close_database(dbref)
+
+
+
+def test_01_10_item_from__partial_dict(open_database):
+    dbref = open_database
+    values = {'record_id': 15}
+    condition = Condition(dbref, values)
+    assert values['record_id'] == condition.get_record_id()
+    assert "" == condition.get_condition()
+    close_database(dbref)
+
+def test_01_11_add(create_conditions_table):
+    dbref = create_conditions_table
+    condition = Condition(dbref, condition_values)
+    record_id = condition.add()
+    assert record_id == 1
+    assert record_id == condition.get_record_id()
+    assert condition_values['condition'] == condition.get_condition()
+    close_database(dbref)
+    
+def test_01_12_read_db(create_conditions_table):
+    dbref = create_conditions_table
+    condition = Condition(dbref)
+    condition.set_properties(condition_values)
+    record_id = condition.add()
+    assert record_id == 1
+        #read db for existing part
+    condition2 = Condition(dbref, record_id)
+    assert record_id == condition2.get_record_id()
+    assert condition_values['condition'] == condition2.get_condition()
+        # read db for non-existing part
+    condition3 = Condition(dbref, 5)
+    assert isinstance(condition3.get_properties(), dict)
+    assert len(condition3.get_properties()) == len(condition_values)
+        # Try direct read thru Element
+    condition2.set_properties(condition2.get_properties_from_db(None, None))
+    assert isinstance (condition2.get_properties(), dict)
+    assert len(condition2.get_properties()) == 0
+    close_database(dbref)
+
+def test_01_12_update(create_conditions_table):
+    dbref = create_conditions_table
+    condition = Condition(dbref)
+    condition.set_properties(condition_values)
+    record_id = condition.add()
+    assert record_id == 1
+    assert condition_values['condition'] == condition.get_condition()
+        
+        # update condition
+    condition.set_condition("British Wiring")
+    result = condition.update()
+    assert result
+    assert condition.get_properties() is not None
+    assert record_id == condition.get_record_id()
+    assert not condition_values['condition'] == condition.get_condition()
+    assert "British Wiring" == condition.get_condition()
+    close_database(dbref)
+
+def test_01_13_delete(create_conditions_table):
+    dbref = create_conditions_table
+    condition = Condition(dbref)
+    condition.set_properties(condition_values)
+    record_id = condition.add()
+        #delete part
+    result = condition.delete()
+    assert result
+        #make sure it is really gone
+    condition2 = Condition(dbref, condition_values['record_id'])
+    assert isinstance (condition2.get_properties(), dict)
+    assert len(condition2.get_properties()) == len(condition_values)
+    close_database(dbref)
+
