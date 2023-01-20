f90a158e53aaa3d5ad4b588fe5ddb92e407917bb
LornBKerr <lornburtkerr@gmail.com>
2023-01-20 10:58:20 -0500

    Update module to reflect Source, SourceSet, Condition, and ConditionSet availability

--- src/elements/__init__.py
+++ src/elements/__init__.py
@@ -2,6 +2,8 @@
 The Parts Database Element Collection
 
 This module contains the following classes
+    Condition extends lbk_library.Element
+    ConditionSet extends lbk_library.ElementSet
     Item extends lbk_library.Element
     ItemSet extends lbk_library.ElementSet
     Part extends lbk_library.Element
@@ -10,18 +12,20 @@ This module contains the following classes
 #    OrderSet extends lbk_library.ElementSet
 #    OrderLine extends lbk_library.Element
 #    OrderLineSet extends lbk_library.ElementSet
-#    Source extends lbk_library.Element
-#    SourceSet extends lbk_library.ElementSet
-#    Condition extends lbk_library.Element
-#    ConditionSet extends lbk_library.ElementSet
-#
+    Source extends lbk_library.Element
+    SourceSet extends lbk_library.ElementSet
+
 File       __init__.py
 Author     Lorn B Kerr
 Copyright  (c) 2023 Lorn B Kerr
 License    MIT (https://opensource.org/licenses/MIT)
 """
 
+from .condition import Condition
+from .condition_set import ConditionSet
 from .item import Item
 from .item_set import ItemSet
 from .part import Part
 from .part_set import PartSet
+from .source import Source
+from .source_set import SourceSet
