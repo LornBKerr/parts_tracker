4505ac80ec44aec76db0b50033e98aaa880864e3
LornBKerr <lornburtkerr@gmail.com>
2023-01-20 12:33:44 -0500

    Class PartSet and associated test file are complete

--- tests/elements/test_08_part_set.py
+++ tests/elements/test_08_part_set.py
@@ -8,36 +8,14 @@ if src_path not in sys.path:
     sys.path.append(src_path)
 
 from lbk_library import Dbal, ElementSet
+from test_setup_elements import (
+    close_database,
+    create_parts_table,
+    database_name,
+    open_database,
+)
 
 from elements import Part, PartSet
-
-database = "parts_test.db"
-
-
-def close_database(dbref):
-    dbref.sql_close()
-
-
-# def delete_database():
-#    os.remove(database)
-
-
-@pytest.fixture
-def create_parts_table(tmpdir):
-    path = tmpdir.join(database)
-    dbref = Dbal()
-    dbref.sql_connect(path)
-    dbref.sql_query("DROP TABLE IF EXISTS 'parts'")
-    create_table = (
-        'CREATE TABLE IF NOT EXISTS "parts" ('
-        + ' "record_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,'
-        + ' "part_number" TEXT NOT NULL,'
-        + ' "source" TEXT NOT NULL,'
-        + ' "description" TEXT NOT NULL,'
-        + ' "remarks" TEXT DEFAULT NULL );'
-    )
-    dbref.sql_query(create_table)
-    return dbref
 
 
 def load_parts_table(dbref):
@@ -47,7 +25,7 @@ def load_parts_table(dbref):
             "1786",
             "X081",
             "Local Purchase",
-            "Rivet, Pop,1/8, Dia, 1/4 grip, Aluminum",
+            "Rivet, Pop,1/8",
             "",
         ],
         ["1787", "X080", "None", "Fenders, Rear", ""],
@@ -62,8 +40,8 @@ def load_parts_table(dbref):
             "1795",
             "282-385",
             "Moss USA",
-            "Grommet, 9/16 OD x 3/16 ID",
-            "License Plate Wires",
+            "Grommet, 9/16x 3/16",
+            "License Plate",
         ],
         ["1796", "324-655", "None", "Washer, Flat, 3/8 ID, 1 1/4 OD, 1/8 Thick", ""],
     ]
@@ -78,45 +56,51 @@ def load_parts_table(dbref):
         dbref.sql_query(sql, entries)
 
 
-def test_08_01_constr(create_parts_table):
-    dbref = create_parts_table
+def test_08_01_constr(open_database):
+    dbref = open_database
+    create_parts_table(dbref)
     part_set = PartSet(dbref)
     assert isinstance(part_set, PartSet)
     assert isinstance(part_set, ElementSet)
     close_database(dbref)
 
 
-def test_08_02_get_dbref(create_parts_table):
-    dbref = create_parts_table
+def test_08_02_get_dbref(open_database):
+    dbref = open_database
+    create_parts_table(dbref)
     part_set = PartSet(dbref)
     assert part_set.get_dbref() == dbref
     close_database(dbref)
 
 
-def test_08_03_get_table(create_parts_table):
-    dbref = create_parts_table
+def test_08_03_get_table(open_database):
+    dbref = open_database
+    create_parts_table(dbref)
     part_set = PartSet(dbref)
     assert part_set.get_table() == "parts"
     close_database(dbref)
 
 
-def test_08_04_set_table(create_parts_table):
-    dbref = create_parts_table
+def test_08_04_set_table(open_database):
+    dbref = open_database
+    create_parts_table(dbref)
     part_set = PartSet(dbref)
     part_set.set_table("items")
     assert part_set.get_table() == "items"
     close_database(dbref)
 
 
-def test_08_05_get_property_set(create_parts_table):
-    dbref = create_parts_table
+def test_08_05_get_property_set(open_database):
+    dbref = open_database
+    create_parts_table(dbref)
     part_set = PartSet(dbref)
     assert isinstance(part_set.get_property_set(), list)
     close_database(dbref)
 
 
-def test_08_06_set_property_set_none(create_parts_table):
-    dbref = create_parts_table
+def test_08_06_set_property_set_none(open_database):
+    dbref = open_database
+    create_parts_table(dbref)
     part_set = PartSet(dbref)
     assert isinstance(part_set.get_property_set(), list)
     part_set.set_property_set(None)
@@ -125,8 +109,9 @@ def test_08_06_set_property_set_none(create_parts_table):
     close_database(dbref)
 
 
-def test_08_07_all_rows_empty(create_parts_table):
-    dbref = create_parts_table
+def test_08_07_all_rows_empty(open_database):
+    dbref = open_database
+    create_parts_table(dbref)
     part_set = PartSet(dbref)
     count_result = dbref.sql_query("SELECT COUNT(*) FROM " + part_set.get_table())
     count = dbref.sql_fetchrow(count_result)["COUNT(*)"]
@@ -136,8 +121,9 @@ def test_08_07_all_rows_empty(create_parts_table):
     close_database(dbref)
 
 
-def test_08_08_selected_rows(create_parts_table):
-    dbref = create_parts_table
+def test_08_08_selected_rows(open_database):
+    dbref = open_database
+    create_parts_table(dbref)
     load_parts_table(dbref)
     part_set = PartSet(dbref, "source", "Victoria British")
     count_result = dbref.sql_query(
@@ -151,49 +137,62 @@ def test_08_08_selected_rows(create_parts_table):
     close_database(dbref)
 
 
-# def test_02_09_ordered_selected_rows(create_items_table):
-#    dbref = create_items_table
-#    load_items_table(dbref)
-#    item_set = ItemSet(dbref,'part_number', 'BTB1108', 'assembly')
-#    count_result = dbref.sql_query("SELECT COUNT(*) FROM " + item_set.get_table() + " WHERE part_number = 'BTB1108'")
-#    count = dbref.sql_fetchrow(count_result)['COUNT(*)']
-#    selected_set = item_set.get_property_set()
-#    assert count == len(selected_set)
-#    for counter in range(0, count - 2):
-#        item1 = selected_set[counter]
-#        item2 = selected_set[counter + 1]
-#        assert item1.get_assembly() < item2.get_assembly()
-#    close_database(dbref)
-#
-# def test_02_10_selected_rows_limit(create_items_table):
-#    dbref = create_items_table
-#    load_items_table(dbref)
-#    limit = 5
-#    item_set = ItemSet(dbref, None, None, 'record_id', limit)
-#    assert limit == len(item_set.get_property_set())
-#    assert item_set.get_property_set()[0].get_record_id() == 1
-#    close_database(dbref)
-#
-# def test_02_11_selected_rows_limit_offset(create_items_table):
-#    dbref = create_items_table
-#    load_items_table(dbref)
-#    limit = 5
-#    offset = 2
-#    item_set = ItemSet(dbref, None, None, 'record_id', limit, offset)
-#    assert limit == len(item_set.get_property_set())
-#    assert item_set.get_property_set()[0].get_record_id() == 3
-#    close_database(dbref)
-#
-# def test_02_12_iterator(create_items_table):
-#    dbref = create_items_table
-#    load_items_table(dbref)
-#    limit = 5
-#    item_set = ItemSet(dbref, None, None, 'record_id', limit)
-#    i = 1
-#    for item in item_set:
-#        assert item.get_record_id() == i
-#        i += 1
-#    close_database(dbref)
+def test_02_09_ordered_selected_rows(open_database):
+    dbref = open_database
+    create_parts_table(dbref)
+    load_parts_table(dbref)
+    part_set = PartSet(dbref, "part_number", "BTB1108")
+    count_result = dbref.sql_query(
+        "SELECT COUNT(*) FROM "
+        + part_set.get_table()
+        + " WHERE part_number = 'BTB1108'"
+    )
+    count = dbref.sql_fetchrow(count_result)["COUNT(*)"]
+    selected_set = part_set.get_property_set()
+    assert count == len(selected_set)
+    for counter in range(0, count - 2):
+        item1 = selected_set[counter]
+        item2 = selected_set[counter + 1]
+        assert item1.get_assembly() < item2.get_assembly()
+    close_database(dbref)
 
 
-# end test_04_elements_part_set.py
+def test_02_10_selected_rows_limit(open_database):
+    dbref = open_database
+    create_parts_table(dbref)
+    load_parts_table(dbref)
+    limit = 5
+    part_set = PartSet(dbref, None, None, "record_id", limit)
+    for part in part_set:
+        print(part.get_properties())
+    assert limit == len(part_set.get_property_set())
+    assert part_set.get_property_set()[0].get_record_id() == 1786
+    close_database(dbref)
+
+
+def test_02_11_selected_rows_limit_offset(open_database):
+    dbref = open_database
+    create_parts_table(dbref)
+    load_parts_table(dbref)
+    limit = 5
+    offset = 2
+    part_set = PartSet(dbref, None, None, "record_id", limit, offset)
+    assert limit == len(part_set.get_property_set())
+    assert part_set.get_property_set()[0].get_record_id() == 1788
+    close_database(dbref)
+
+
+def test_02_12_iterator(open_database):
+    dbref = open_database
+    create_parts_table(dbref)
+    load_parts_table(dbref)
+    limit = 5
+    part_set = PartSet(dbref, None, None, "record_id", limit)
+    i = 1
+    for item in part_set:
+        assert item.get_record_id() == 1786 + i - 1
+        i += 1
+    close_database(dbref)
+
+
+# end test_08_part_set.py
