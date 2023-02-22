8bb632679b96a62d027d7c969dc3663a62a307da
LornBKerr <lornburtkerr@gmail.com>
2023-02-22 11:42:13 -0500

    added handling of system exit commands

--- src/parts_tracker.py
+++ src/parts_tracker.py
@@ -1,10 +1,10 @@
 """
-Run the 'Parts Tracker' Application
+Run the 'Parts Tracker' Application.
 
 File:       PartsTracker.py
 Author:     Lorn B Kerr
 Copyright:  (c) 2022, 2023 Lorn B Kerr
-License:    MIT, see file License 
+License:    MIT, see file License
 """
 
 import sys
@@ -16,4 +16,7 @@ from pages import MainWindow
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main_window = MainWindow(app)
+    # handle the system close button and exit commands
+    app.aboutToQuit.connect(main_window.exit_app_action)
+
     sys.exit(app.exec())
