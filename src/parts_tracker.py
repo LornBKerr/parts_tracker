45972a2d20239db766ec652670386fc816f87ba8
LornBKerr <lornburtkerr@gmail.com>
2023-04-03 12:55:32 -0400

    Remove unused parameter 'app' from call to main_window.

--- src/parts_tracker.py
+++ src/parts_tracker.py
@@ -15,8 +15,7 @@ from pages import MainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    main_window = MainWindow(app)
+    main_window = MainWindow()
     # handle the system close button and exit commands
     app.aboutToQuit.connect(main_window.exit_app_action)
-
     sys.exit(app.exec())
