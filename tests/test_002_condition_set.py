bb1a76ca00595eb1d5dd231bac6152e16dcbd48f
LornBKerr <lornburtkerr@gmail.com>
2023-04-03 13:01:50 -0400

    Replace all calls to load_conditions_table() with calls to laod_db_table() and add imports 'condition_columns' and 'condition_value_set'; rename all test function from def test_02_... to def test_002_...

--- tests/test_002_condition_set.py
+++ tests/test_002_condition_set.py
@@ -12,7 +12,14 @@ import sys
 
 import pytest
 from lbk_library import Dbal, ElementSet
-from test_setup import db_close, db_create, db_open, load_conditions_table
+from test_setup import (
+    condition_columns,
+    condition_value_set,
+    db_close,
+    db_create,
+    db_open,
+    load_db_table,
+)
 
 src_path = os.path.join(os.path.realpath("."), "src")
 if src_path not in sys.path:
@@ -29,21 +36,21 @@ def test_002_01_constr(db_create):
     db_close(dbref)
 
 
-def test_02_02_get_dbref(db_create):
+def test_002_02_get_dbref(db_create):
     dbref = db_create
     condition_set = ConditionSet(dbref)
     assert condition_set.get_dbref() == dbref
     db_close(dbref)
 
 
-def test_02_03_get_table(db_create):
+def test_002_03_get_table(db_create):
     dbref = db_create
     condition_set = ConditionSet(dbref)
     assert condition_set.get_table() == "conditions"
     db_close(dbref)
 
 
-def test_02_04_set_table(db_create):
+def test_002_04_set_table(db_create):
     dbref = db_create
     condition_set = ConditionSet(dbref)
     condition_set.set_table("items")
@@ -51,14 +58,14 @@ def test_02_04_set_table(db_create):
     db_close(dbref)
 
 
-def test_02_05_get_property_set(db_create):
+def test_002_05_get_property_set(db_create):
     dbref = db_create
     condition_set = ConditionSet(dbref)
     assert isinstance(condition_set.get_property_set(), list)
     db_close(dbref)
 
 
-def test_02_06_set_property_set_none(db_create):
+def test_002_06_set_property_set_none(db_create):
     dbref = db_create
     condition_set = ConditionSet(dbref)
     assert isinstance(condition_set.get_property_set(), list)
@@ -68,7 +75,7 @@ def test_02_06_set_property_set_none(db_create):
     db_close(dbref)
 
 
-def test_02_07_all_rows_empty(db_create):
+def test_002_07_all_rows_empty(db_create):
     dbref = db_create
     conditions_set = ConditionSet(dbref)
     count_result = dbref.sql_query("SELECT COUNT(*) FROM " + conditions_set.get_table())
@@ -78,9 +85,9 @@ def test_02_07_all_rows_empty(db_create):
     db_close(dbref)
 
 
-def test_02_08_selected_rows(db_create):
+def test_002_08_selected_rows(db_create):
     dbref = db_create
-    load_conditions_table(dbref)
+    load_db_table(dbref, "conditions", condition_columns, condition_value_set)
     conditions_set = ConditionSet(dbref, "condition", "Missing")
     count_result = dbref.sql_query(
         "SELECT COUNT(*) FROM "
