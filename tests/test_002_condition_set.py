60ccee926b987a421f69a05f9383df7689e3677f
LornBKerr <lornburtkerr@gmail.com>
2023-12-29 13:29:14 -0500

    Expanded tests relating to properties, removed redundant database tests, corrected file permissions.

--- tests/test_002_condition_set.py
+++ tests/test_002_condition_set.py
@@ -17,6 +17,7 @@ from test_setup import (
     db_close,
     db_create,
     db_open,
+    filesystem,
     load_db_table,
 )
 
@@ -27,73 +28,57 @@ if src_path not in sys.path:
 from elements import ConditionSet
 
 
-def test_002_01_constr(db_create):
-    dbref = db_create
+def test_002_01_constr(filesystem):
+    """
+    ConditionSet Extends ElementSet.
+
+    The 'table' must be "conditions" and 'dbref' needs to be the
+    initializing dbref.
+    """
+    fs_base = filesystem
+    dbref = db_create(fs_base)
     condition_set = ConditionSet(dbref)
     assert isinstance(condition_set, ConditionSet)
     assert isinstance(condition_set, ElementSet)
-    db_close(dbref)
-
-
-def test_002_02_get_dbref(db_create):
-    dbref = db_create
-    condition_set = ConditionSet(dbref)
+    assert condition_set.get_table() == "conditions"
     assert condition_set.get_dbref() == dbref
     db_close(dbref)
 
 
-def test_002_03_get_table(db_create):
-    dbref = db_create
-    condition_set = ConditionSet(dbref)
-    assert condition_set.get_table() == "conditions"
-    db_close(dbref)
-
-
-def test_002_04_set_table(db_create):
-    dbref = db_create
-    condition_set = ConditionSet(dbref)
-    condition_set.set_table("items")
-    assert condition_set.get_table() == "items"
-    db_close(dbref)
-
-
-def test_002_05_get_property_set(db_create):
-    dbref = db_create
-    condition_set = ConditionSet(dbref)
-    assert isinstance(condition_set.get_property_set(), list)
-    db_close(dbref)
-
-
-def test_002_06_set_property_set_none(db_create):
-    dbref = db_create
+def test_002_02_set_property_set_empty(filesystem):
+    """
+    The 'property_set', a list of 'Conmditions'", is empty when set to
+    None or when the table is empty.
+    """
+    fs_base = filesystem
+    dbref = db_create(fs_base)
     condition_set = ConditionSet(dbref)
     assert isinstance(condition_set.get_property_set(), list)
     condition_set.set_property_set(None)
     assert isinstance(condition_set.get_property_set(), list)
     assert len(condition_set.get_property_set()) == 0
-    db_close(dbref)
-
-
-def test_002_07_all_rows_empty(db_create):
-    dbref = db_create
-    conditions_set = ConditionSet(dbref)
-    count_result = dbref.sql_query("SELECT COUNT(*) FROM " + conditions_set.get_table())
+    count_result = dbref.sql_query("SELECT COUNT(*) FROM " + condition_set.get_table())
     count = dbref.sql_fetchrow(count_result)["COUNT(*)"]
-    assert count == len(conditions_set.get_property_set())
-    assert count == conditions_set.get_number_elements()
+    assert count == len(condition_set.get_property_set())
+    assert count == condition_set.get_number_elements()
     db_close(dbref)
 
 
-def test_002_08_selected_rows(db_create):
-    dbref = db_create
+def test_002_03_selected_rows(filesystem):
+    """
+    The 'property_set', a list of Conmditions", should contain the
+    requested subset of conditions.
+    """
+    fs_base = filesystem
+    dbref = db_create(fs_base)
     load_db_table(dbref, "conditions", condition_columns, condition_value_set)
-    conditions_set = ConditionSet(dbref, "condition", "Missing")
+    condition_set = ConditionSet(dbref, "condition", "Missing")
     count_result = dbref.sql_query(
         "SELECT COUNT(*) FROM "
-        + conditions_set.get_table()
+        + condition_set.get_table()
         + " WHERE condition = 'Missing'"
     )
     count = dbref.sql_fetchrow(count_result)["COUNT(*)"]
-    assert count == len(conditions_set.get_property_set())
+    assert count == len(condition_set.get_property_set())
     assert count == 1
     db_close(dbref)
