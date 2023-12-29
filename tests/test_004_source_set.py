52af9552759a7d41a3d38792bbac544a138c6c67
LornBKerr <lornburtkerr@gmail.com>
2023-12-29 13:31:19 -0500

    Expanded tests relating to properties, removed redundant database tests, corrected file permissions.

--- tests/test_004_source_set.py
+++ tests/test_004_source_set.py
@@ -10,12 +10,12 @@ License:    MIT, see file License
 import os
 import sys
 
-import pytest
 from lbk_library import Dbal, ElementSet
 from test_setup import (
     db_close,
     db_create,
     db_open,
+    filesystem,
     load_db_table,
     source_columns,
     source_value_set,
@@ -28,73 +28,57 @@ if src_path not in sys.path:
 from elements import SourceSet
 
 
-def test_004_01_constr(db_create):
-    dbref = db_create
+def test_004_01_constr(filesystem):
+    """
+    SourceSet Extends ElementSet.
+
+    The 'table' must be "conditions" and 'dbref' needs to be the
+    initializing dbref.
+    """
+    fs_base = filesystem
+    dbref = db_create(fs_base)
     source_set = SourceSet(dbref)
     assert isinstance(source_set, SourceSet)
     assert isinstance(source_set, ElementSet)
-    db_close(dbref)
-
-
-def test_004_02_get_dbref(db_create):
-    dbref = db_create
-    source_set = SourceSet(dbref)
+    assert source_set.get_table() == "sources"
     assert source_set.get_dbref() == dbref
     db_close(dbref)
 
 
-def test_004_03_get_table(db_create):
-    dbref = db_create
-    source_set = SourceSet(dbref)
-    assert source_set.get_table() == "sources"
-    db_close(dbref)
-
-
-def test_004_04_set_table(db_create):
-    dbref = db_create
-    source_set = SourceSet(dbref)
-    source_set.set_table("items")
-    assert source_set.get_table() == "items"
-    db_close(dbref)
-
-
-def test_004_05_get_property_set(db_create):
-    dbref = db_create
-    source_set = SourceSet(dbref)
-    assert isinstance(source_set.get_property_set(), list)
-    db_close(dbref)
-
-
-def test_004_06_set_property_set_none(db_create):
-    dbref = db_create
+def test_004_02_set_property_set_empty(filesystem):
+    """
+    The 'property_set', a list of 'Sources', is empty when set to
+    None or when the table is empty.
+    """
+    fs_base = filesystem
+    dbref = db_create(fs_base)
     source_set = SourceSet(dbref)
     assert isinstance(source_set.get_property_set(), list)
     source_set.set_property_set(None)
     assert isinstance(source_set.get_property_set(), list)
     assert len(source_set.get_property_set()) == 0
-    db_close(dbref)
-
-
-def test_004_07_all_rows_empty(db_create):
-    dbref = db_create
-    sources_set = SourceSet(dbref)
-    count_result = dbref.sql_query("SELECT COUNT(*) FROM " + sources_set.get_table())
+    count_result = dbref.sql_query("SELECT COUNT(*) FROM " + source_set.get_table())
     count = dbref.sql_fetchrow(count_result)["COUNT(*)"]
-    assert count == len(sources_set.get_property_set())
-    assert count == sources_set.get_number_elements()
+    assert count == len(source_set.get_property_set())
+    assert count == source_set.get_number_elements()
     db_close(dbref)
 
 
-def test_004_08_selected_rows(db_create):
-    dbref = db_create
+def test_004_03_selected_rows(filesystem):
+    """
+    The 'property_set', a list of 'Sources', should contain the
+    requested subset of conditions.
+    """
+    fs_base = filesystem
+    dbref = db_create(fs_base)
     load_db_table(dbref, "sources", source_columns, source_value_set)
-    sources_set = SourceSet(dbref, "source", "British Car Parts")
+    source_set = SourceSet(dbref, "source", "British Car Parts")
     count_result = dbref.sql_query(
         "SELECT COUNT(*) FROM "
-        + sources_set.get_table()
+        + source_set.get_table()
         + " WHERE source = 'British Car Parts'"
     )
     count = dbref.sql_fetchrow(count_result)["COUNT(*)"]
-    assert count == len(sources_set.get_property_set())
+    assert count == len(source_set.get_property_set())
     assert count == 1
     db_close(dbref)
