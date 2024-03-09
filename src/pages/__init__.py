631322218a9bf1708ef2f994172b51376cfab9d6
LornBKerr <lornburtkerr@gmail.com>
2024-03-09 15:50:48 -0500

    Updated with the main window and stubs for the tree and table contents.

--- src/pages/__init__.py
+++ src/pages/__init__.py
@@ -2,18 +2,24 @@
 The pages collection providing the pages for the main window.
 
 This module contains the following classes:
-     MainWindow extends QMainWindow)
+    MainWindow (QMainWindow): The Core window into the program.
+    AssemblyTreePage (QWidget): Displays the Items in a Tree listing
+        format.
+    PartsListPaage (QObject): Displays the Parts in a Table listing.
+    OrderListPage (OQject): Displays the Orders in a Table Listing.
+
+Also included is:
+    table_definition (List[str]): A list of sql definitions for the 
+        Parts Database file.
 
 File       __init__.py
 Author     Lorn B Kerr
-Copyright  (c) 2023 Lorn B Kerr
+Copyright  (c) 2023, 2024 Lorn B Kerr
 License:    MIT, see file License
 """
 
 from .assembly_tree_page import AssemblyTreePage
 from .main_window import MainWindow
-
-#
-# from .orders_list_page import OrdersListPage
-# from .parts_list_page import PartsListPage
-# from .parts_table_definition import table_definition
+from .orders_list_page import OrdersListPage
+from .parts_list_page import PartsListPage
+from .parts_table_definition import table_definition
