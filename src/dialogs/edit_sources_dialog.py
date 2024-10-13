291470feba17241289e6bce912cdaeb2ed6cec0b
LornBKerr <lornburtkerr@gmail.com>
2024-10-13 12:46:41 -0400

    Added the edit sources capability.

---
+++ src/dialogs/edit_sources_dialog.py
@@ -0,0 +1,193 @@
+"""
+Edit the set of possible Part source.
+
+File:       edit_sources_dialog.py
+Author:     Lorn B Kerr
+Copyright:  (c) 2024 Lorn B Kerr
+License:    MIT, see file License
+Version:    1.0.0
+"""
+
+from lbk_library import DataFile
+from lbk_library.gui import Dialog, TableModel
+from PyQt5 import uic
+from PyQt5.QtCore import QModelIndex, Qt
+from PyQt5.QtGui import QBrush, QColor
+from PyQt5.QtWidgets import QHeaderView
+
+from elements import Source, SourceSet
+
+file_version = "1.0.0"
+changes = {
+    "1.0.0": "Initial release",
+}
+
+
+class EditSourcesDialog(Dialog):
+    """
+    Edit the set of possible Part sources.
+
+    Each part in the data base has a source assigned. In general, the
+    source is where the part was purchsed
+    """
+
+    ALIGNMENTS = [
+        Qt.AlignmentFlag.AlignLeft,
+        Qt.AlignmentFlag.AlignLeft,
+    ]
+    """The alignments for each of the columns."""
+    COLUMN_NAMES = ["record_id", "source"]
+    """The data names for each of the columns."""
+    COLUMN_WIDTHS = [70, 130]
+    """The initial widths for each column."""
+    HEADER_TITLES = ["Record Id", "Source"]
+    """The titles for each of the columns."""
+    TOOLTIPS = [
+        "Record Id is automatically set.",
+        "Required: Edit or add a part source,",
+    ]
+    """Tooltips for each of the visible elements on the form."""
+    NORMAL_BACKGROUND = QBrush(QColor("white"))
+    ERROR_BACKGROUND = QBrush(QColor(0xF0C0C0))
+
+    def __init__(self, parts_file: DataFile) -> None:
+        """
+        Initialize the dialog.
+
+        Parameters:
+            parts_file (DataFile) reference to the current open data file.
+        """
+        super().__init__(None, parts_file, None)
+        self.parts_file = parts_file
+        self.form = uic.loadUi("./src/forms/simple_tableview.ui", self)
+        self.sources = SourceSet(parts_file)
+        self.source_list = self.sources.get_property_set()
+        self.dataset = self.build_data_set(self.sources)
+
+        self.table = self.form.table_view
+        self.model = TableModel(
+            self.dataset,
+            self.HEADER_TITLES,
+            self.TOOLTIPS,
+            self.ALIGNMENTS,
+            self.NORMAL_BACKGROUND,
+        )
+        self.table.setModel(self.model)
+        self.table.setStyleSheet(
+            "QTableView {selection-background-color: white; selection-color: blue;}"
+        )
+
+        self.setup_form()
+        self.append_row()
+
+        self.record_id_checkbox.stateChanged.connect(self.show_record_id)
+        self.model.dataChanged.connect(self.data_changed)
+        self.complete_button.clicked.connect(self.close_form)
+
+        # this is used in processing the 'dataChanged" signal
+        self.__change_in_process = False
+
+    def build_data_set(self, source_set: SourceSet) -> list[list[str]]:
+        """
+        Convert the properties of the SourceSet to an array.
+
+        Parameters:
+            source_set (SourceSet): the set to convert to an array
+
+        Returns:
+            (list[list[str]]) The set of source properties as an
+                array of strings.
+        """
+        data_set = []
+        for i in range(len(self.source_list)):
+            a_source = []
+            source_properties = self.source_list[i].get_properties()
+            for name in self.COLUMN_NAMES:
+                a_source.append(source_properties[name])
+            data_set.append(a_source)
+        return data_set
+
+    def setup_form(self) -> None:
+        """Configure the table."""
+        self.form.setWindowTitle("Edit Part Sources")
+        self.form.form_label.setText(
+            "<b>Add</b> or <b>Edit</b> the set of Part Sources."
+        )
+
+        font = self.form.copyright_label.font()
+        font.setPointSize(7)
+        self.form.copyright_label.setFont(font)
+
+        self.table.verticalHeader().hide()
+        self.table.setColumnHidden(self.COLUMN_NAMES.index("record_id"), True)
+        for i in range(len(self.COLUMN_WIDTHS)):
+            self.table.setColumnWidth(i, self.COLUMN_WIDTHS[i])
+        self.table.horizontalHeader().setSectionResizeMode(
+            self.HEADER_TITLES.index("Source"), QHeaderView.ResizeMode.Stretch
+        )
+
+    def append_row(self) -> None:
+        """
+        Append an empty row to the data_set.
+
+        Each column value of this empty line will have the value None.
+        """
+        self.model.insertRows(self.model.rowCount(), 1)
+        self.model.layoutChanged.emit()
+
+    def show_record_id(self) -> None:
+        """Show or hide the 'record_id' column in the table."""
+        if self.record_id_checkbox.isChecked():
+            self.table.setColumnHidden(self.COLUMN_NAMES.index("record_id"), False)
+        else:
+            self.table.setColumnHidden(self.COLUMN_NAMES.index("record_id"), True)
+
+    def data_changed(self, index: QModelIndex, index2: QModelIndex) -> None:
+        """
+        Validate the change in a table entry.
+
+        Parameters:
+            index (QmodelIndex): the first changed table cell.
+            index2 (QModelIndex): the last changed table cell. (not used)
+        """
+        if self.__change_in_process:
+            return
+        else:
+            self.__change_in_process = True
+            test_result = Source(self.parts_file).set_source(
+                self.model.data(index, Qt.ItemDataRole.EditRole)
+            )
+
+            if test_result["valid"]:
+                if index.row() < len(self.source_list):
+                    self.source_list[index.row()].set_condition(test_result["entry"])
+                    self.source_list[index.row()].update()
+                else:
+                    new_source = Source(self.parts_file)
+                    new_source.set_source(test_result["entry"])
+                    new_source.add()
+
+                self.model.setData(
+                    index,
+                    self.TOOLTIPS[index.column()],
+                    Qt.ItemDataRole.ToolTipRole,
+                )
+                self.model.setData(
+                    index, self.NORMAL_BACKGROUND, Qt.ItemDataRole.BackgroundRole
+                )
+                self.append_row()
+            else:
+                self.model.setData(
+                    index,
+                    test_result["msg"] + ", " + self.TOOLTIPS[index.column()],
+                    Qt.ItemDataRole.ToolTipRole,
+                )
+                self.model.setData(
+                    index, self.ERROR_BACKGROUND, Qt.ItemDataRole.BackgroundRole
+                )
+
+            self.__change_in_process = False
+
+    def close_form(self) -> None:
+        """Close the form when the "close" button is clicked."""
+        self.close()
---
+++ tests/test_107_edit_sources_dialog.py
@@ -0,0 +1,95 @@
+"""
+Test the EditSourcessDialog. class.
+
+File:       test_1xx_edit_sources_dialog.py
+Author:     Lorn B Kerr
+Copyright:  (c) 2024 Lorn B Kerr
+License:    MIT, see file LICENSE
+Version:    1.0.0
+"""
+
+import os
+import sys
+
+src_path = os.path.join(os.path.realpath("."), "src")
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+from lbk_library.gui import Dialog
+from lbk_library.testing_support import datafile_close, datafile_create, filesystem
+from test_setup import datafile_name, load_all_datafile_tables
+
+from dialogs import EditSourcesDialog
+from elements import SourceSet
+from pages import table_definition
+
+file_version = "1.0.0"
+changes = {
+    "1.0.0": "Initial release",
+}
+header_names = ["Record Id", "Source"]
+
+
+def setup_table_tests(qtbot, filesystem):
+    filename = filesystem + "/" + datafile_name
+    datafile = datafile_create(filename, table_definition)
+    load_all_datafile_tables(datafile)
+    data_set = SourceSet(datafile)
+    dialog = EditSourcesDialog(datafile)
+    qtbot.addWidget(dialog)
+    return (dialog, datafile, data_set)
+
+
+def test_107_01_class_type(qtbot, filesystem):
+    dialog, datafile, data_set = setup_table_tests(qtbot, filesystem)
+
+    assert isinstance(dialog, EditSourcesDialog)
+    assert isinstance(dialog, Dialog)
+    assert isinstance(dialog.sources, SourceSet)
+    for i in range(len(header_names)):
+        assert dialog.model._header_titles[i] == header_names[i]
+    datafile_close(datafile)
+
+
+def test_107_02_build_data_set(qtbot, filesystem):
+    dialog, datafile, data_set = setup_table_tests(qtbot, filesystem)
+
+    dataset = dialog.build_data_set(dialog.sources)
+    set = dialog.sources.get_property_set()
+    for row in range(len(set)):
+        for column in range(len(set[row].get_properties())):
+            assert dataset[row][column] == set[row]._get_property(
+                dialog.COLUMN_NAMES[column]
+            )
+
+
+def test_107_03_setup(qtbot, filesystem):
+    dialog, datafile, data_set = setup_table_tests(qtbot, filesystem)
+
+    assert dialog.form.windowTitle() == "Edit Part Sources"
+    assert (
+        dialog.form_label.text() == "<b>Add</b> or <b>Edit</b> the set of Part Sources."
+    )
+
+
+def test_107_04_append_row(qtbot, filesystem):
+    dialog, parts_file, data_set = setup_table_tests(qtbot, filesystem)
+
+    row_count = dialog.model.rowCount()
+    dialog.append_row()
+    assert dialog.model.rowCount() == row_count + 1
+
+
+def test_107_05_show_record_id(qtbot, filesystem):
+    dialog, datafile, data_set = setup_table_tests(qtbot, filesystem)
+
+    assert not dialog.form.record_id_checkbox.isChecked()
+    assert dialog.table.isColumnHidden(dialog.COLUMN_NAMES.index("record_id"))
+
+    dialog.form.record_id_checkbox.click()
+    assert dialog.form.record_id_checkbox.isChecked()
+    assert not dialog.table.isColumnHidden(dialog.COLUMN_NAMES.index("record_id"))
+
+    dialog.form.record_id_checkbox.click()
+    assert not dialog.form.record_id_checkbox.isChecked()
+    assert dialog.table.isColumnHidden(dialog.COLUMN_NAMES.index("record_id"))
