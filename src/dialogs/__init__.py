e1e102e5bd241f3915b14224a1c7b1928288aadd
LornBKerr <lornburtkerr@gmail.com>
2024-10-31 12:28:18 -0400

    Updated imports to reflect currrent set of actions.

--- src/dialogs/__init__.py
+++ src/dialogs/__init__.py
@@ -2,11 +2,14 @@
 The PartsTracker Dialogs Collection.
 
 This module contains the following classes:
-    BaseDialog extends lbk_library.Dialog - Base class for the set
-        of dialogs.
-    EditStructureDialog extends BaseDialog) - Change the Assembly structure.
-    ItemDialog extends BaseDialog - Edit an Item in the database.
-    PartDialog(BaseDialog) - Edit a Part in the database.
+    AssemblyListDialog - Write a Comma Separated Values (CSV) file of
+        selected portions of the listing
+    BaseDialog - Base class for all dialogs.
+    EditConditionsDialog - Change the set of available Item Conditions
+    EditSourcesDialog - Edit the set of Part Sources 
+    ItemDialog - Edit an Item in the parts file.
+    PartDialog - Edit a Part in the parts file.
+    OrderDialog - Edit an Order in the parts file.
 
 File       __init__.py
 Author     Lorn B Kerr
@@ -15,28 +18,13 @@ License:    MIT, see file License
 Version:    1.0.0
 """
 
-# SaveAssyListDialog -  Write an Excel (XLSX) compatible or Comma Separated
-#                         Values (CSV) file of selected portions of the listing
-#   ChangePartNumberDialog - Change a Part Number in the database.
-
 from .assembly_list_dialog import AssemblyListDialog
 from .base_dialog import BaseDialog
-from .change_part_number_dialog import ChangePartNumberDialog
 from .edit_conditions_dialog import EditConditionsDialog
 from .edit_sources_dialog import EditSourcesDialog
-from .edit_structure_dialog import EditStructureDialog
 from .item_dialog import ItemDialog
 from .order_dialog import OrderDialog
 from .part_dialog import PartDialog
-
-# from .conditions_table_model import ConditionsTableModel
-# from .element_table_model import ElementTableModel
-# from .sources_table_model import SourcesTableModel
-
-# from .comboboxdelegate import ComboBoxDelegate
-# from .table_button import TableButton
-# from .table_button_group import TableButtonGroup
-# from .table_model import TableModel
 
 file_version = "1.0.0"
 changes = {
